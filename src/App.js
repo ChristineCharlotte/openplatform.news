a57ff21a858b33edbcbfc94253fd80e32352f26c
XexT <faaa@live.com>
2023-12-09 13:55:42 +0800

    minor change

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ export default function App() {
     <div>
       <h1>openplatform.news</h1>
       <p>《新闻女王》公开平台 :) 即将推出...</p>
-      <img src="/screenshot.png" alt="" style={{ width: '100%' }} />
       <p>
         一个普通的剧迷建，想认识我？来我的{' '}
         <a href="https://www.xiaohongshu.com/user/profile/5b2862ee11be101a7032eba0?xhsshare=CopyLink&appuid=5b2862ee11be101a7032eba0&apptime=1702100566">
           小红薯
         </a>
       </p>
+      <img src="/screenshot.png" alt="" style={{ width: '100%' }} />
+
     </div>
   );
 }
